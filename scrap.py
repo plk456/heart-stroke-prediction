a4f60b6139c04bd4ebc6f1ad82512011c5f514ab
Likith kumar <kumarlikith178@gmail.com>
2025-06-22 10:39:02 +0530

    updated_vesion

--- scrap.py
+++ scrap.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 
 content=requests.get('https://quotes.toscrape.com/')   
-#print(content)                     
+print(content)                     
 soup=BeautifulSoup(content.text,'html.parser')
 print(soup)                         # This statement will print the entire HTML content of the website
 
-#s=soup.find_all('a')               # Remove the comments to scrape the anchor tag <a> href link               
-#for i in s:                                    
-#    href = i.get('href')
-#    print(href)
+s=soup.find_all('a')               # Remove the comments to scrape the anchor tag <a> href link               
+for i in s:                                    
+    href = i.get('href')
+    print(href)
 
 
 
